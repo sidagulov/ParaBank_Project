4ad4228fb8f9e5faf3afdb63988d47896e7a7e7f
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-09 16:47:15 +0200

    steps are defined

---
+++ src/test/java/StepDefinitions/TC_08_LoanApplication.java
@@ -0,0 +1,38 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+
+public class TC_08_LoanApplication {
+    @And("user click on {string} link")
+    public void userClickOnLink(String arg0) {
+    }
+
+    @And("user enters {string} in the {string} field")
+    public void userEntersInTheField(String arg0, String arg1) {
+    }
+
+    @And("user enters {string} dollars in {string} field")
+    public void userEntersDollarsInField(String arg0, String arg1) {
+    }
+
+    @And("user selects an account number in the {string}")
+    public void userSelectsAnAccountNumberInThe(String arg0) {
+    }
+
+    @And("user click on the {string} button")
+    public void userClickOnTheButton(String arg0) {
+    }
+
+    @And("user confirms the following information:")
+    public void userConfirmsTheFollowingInformation() {
+    }
+
+    @When("user Click on the link of the newly created credit account number")
+    public void userClickOnTheLinkOfTheNewlyCreatedCreditAccountNumber() {
+    }
+
+    @And("user {string} confirms the message")
+    public void userConfirmsTheMessage(String arg0) {
+    }
+}
